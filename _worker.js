c47754e05a19a07a854d2b953cfd7a7fe4922573
superencrypt-dev <283459715+superencrypt-dev@users.noreply.github.com>
2026-09-22 02:25:25 +0800

    feat: point Pages静态页面 to EN frontend (edt-pages-en)

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 ﻿const Version = '2026-09-04 16:24:13';
 let config_JSON, 缓存SOCKS5白名单 = null, 调试日志打印 = false;
 let SOCKS5白名单 = ['*tapecontent.net', '*cloudatacdn.com', '*loadshare.org', '*cdn-centaurus.com', 'scholar.google.com'];
-const Pages静态页面 = 'https://edt-pages.github.io';
+const Pages静态页面 = 'https://edt-pages-en.superencrypt.workers.dev';
 ///////////////////////////////////////////////////////全局常量和工具函数///////////////////////////////////////////////
 const WS早期数据最大字节 = 8 * 1024, WS早期数据最大头长度 = Math.ceil(WS早期数据最大字节 * 4 / 3) + 4;
 const 上行合包目标字节 = 20 * 1024, 上行队列最大字节 = 16 * 1024 * 1024, 上行队列最大条目 = 4096;
